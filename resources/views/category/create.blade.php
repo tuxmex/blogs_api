3ce6eb31b652f660d54520aa3dca43a9a37c4814
Tacho Rodriguez Garcia <tuxmex@MacBook-Air-de-Tacho.local>
2021-07-02 17:22:02 -0500

    starting

---
+++ resources/views/category/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layout')
+@section('dashboard-content')
+    <h1>Create category form</h1>
+        @if (Session::get('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert" id="gone">
+                <strong>{{Session::get('success')}}</strong>
+                <button type="button" class="close" data-dismis="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
+        @if (Session::get('failed'))
+        <div class="alert alert-warning alert-dismissible fade show" role="alert" id="gone">
+            <strong>{{Session::get('failed')}}</strong>
+            <button type="button" class="close" data-dismis="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    @endif            
+
+        <form action="{{URL::to('post-category-form')}}" method="post">
+            @csrf
+            <div class="form-group">
+              <label for="inputCategoryName">Category name</label>
+              <input type="text" class="form-control" id="inputCategoryName" 
+               placeholder="Enter category name"
+              name="categoryName">            
+            </div>
+            <button type="submit" class="btn btn-primary mt-3">Submit</button>
+           
+          </form>
+@stop
